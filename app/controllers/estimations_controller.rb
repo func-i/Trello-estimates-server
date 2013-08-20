ffade8e7dfdcf1e86caaaf79e999bcddfddaf200
Bruno Meira <goesmeira@gmail.com>
2013-08-20 01:34:27 -0400

    -Fix code due changes on trello URL

--- app/controllers/estimations_controller.rb
+++ app/controllers/estimations_controller.rb
@@ -5,17 +5,17 @@ class EstimationsController < ApplicationController
   end
 
   def create
-    board_id = params[:estimation][:board_id]
     card_id = params[:estimation][:card_id]
     user_username = params[:estimation].delete :user_username
     is_manager = params[:estimation][:is_manager].to_bool
 
     user = current_user.find(:member, user_username)
+    card = current_user.find(:cards, ""+card_id)
 
     if is_manager
-      estimation_db = Estimation.where(:board_id => board_id, :card_id => card_id, :is_manager => is_manager).first
+      estimation_db = Estimation.where(:board_id => card.board_id, :card_id => card.id, :is_manager => is_manager).first
     else
-      estimation_db = Estimation.where(:board_id => board_id, :card_id => card_id, :user_id => user.id, :is_manager => is_manager).first
+      estimation_db = Estimation.where(:board_id => card.board_id, :card_id => card.id, :user_id => user.id, :is_manager => is_manager).first
     end
 
     estimation = if estimation_db
