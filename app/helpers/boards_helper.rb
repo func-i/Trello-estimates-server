fd7d7c5c74631750528f0000f13aae75d3a1c75d
coolmatt <themattemmons@gmail.com>
2013-08-30 16:29:51 -0400

    Card 2 - refactoring much of the Board#show logic into helper, far from perfect but easier to reason about now

--- app/helpers/boards_helper.rb
+++ app/helpers/boards_helper.rb
@@ -1,4 +1,5 @@
 module BoardsHelper
+
   #TODO Remove SQL calls from helpers
   def time_track_less_estimation(board, lists, only_staged_and_live = false)
     cards = list_cards(lists, only_staged_and_live)
@@ -17,48 +18,63 @@ module BoardsHelper
     end
   end
 
-  def get_developer_estimate(board, card)
-    avg = 0
-    card_id, board_id = get_card_id(card), board.id
-    estimate = Estimation.developers_estimation(@board.id, card_id)
-    if estimate.count > 0
-      avg = estimate.sum(&:user_time) / estimate.count.to_f
-    end
-    sprintf("%.2f", avg)
-  end
-
   private
 
-  def get_card_id(card)
-    card.url.split('/')[4]
-  end
-
-  # <% developers_estimation = Estimation.developers_estimation(@board.id, get_card_id(card)) %>
-
-
-  # <% average_developer_estimation = 0 %>
-  # <% if developers_estimation.count > 0 %>
-  #   <% average_developer_estimation = developers_estimation.sum(&:user_time)/ developers_estimation.count.to_f %>
-  # <% end %>
-  # <%= "%.2f" % average_developer_estimation %>
-
-  def linked_card_name(card)
-    link_to "Card #{card.short_id}", card.url
-  end
-
-  def truncate_card_title(card)
-    truncate card.name, length: 25, seperator: " "
-  end
-
-  def list_time_tracked(board, cards_id)
-    HarvestLog.total_time_tracked(board.id).where("harvest_logs.card_id IN (?)", cards_id)
-  end
-
-  def list_cards(lists, only_staged_and_live)
-    cards = Array.new
-    lists.each do |list|
-      cards+= list.cards.map(&:id) if only_staged_and_live == list.finished?
+    def get_card_id(card)
+      card.url.split('/')[4]
     end
-    cards
-  end
+
+    def get_developer_estimate(board, card)
+      avg = 0
+      card_id, board_id = get_card_id(card), board.id
+      estimate = Estimation.developers_estimation(board_id, card_id)
+      avg = if estimate.count > 0
+        estimate.sum(&:user_time) / estimate.count.to_f
+      else
+        0
+      end
+    end
+
+    def get_manager_estimate(board, card)
+      avg = 0
+      card_id, board_id = get_card_id(card), board.id
+      estimate = Estimation.managers_estimation(board_id, card_id)
+      avg = if estimate.count > 0
+        estimate.sum(&:user_time) / estimate.count.to_f
+      else
+        0
+      end
+    end
+
+    def developer_time_ratio(board, card)
+      total_time = total_harvest_time(board, card)
+      get_developer_estimate(board, card) - total_time
+    end
+
+    def manager_time_ratio(board, card)
+      total_time = total_harvest_time(board, card)
+      get_manager_estimate(board, card) - total_time
+    end
+
+    def linked_card_name(card)
+      link_to "Card #{card.short_id} - #{truncate card.name, length: 40}", card.url
+    end
+
+    def list_time_tracked(board, cards_id)
+      HarvestLog.total_time_tracked(board.id).where("harvest_logs.card_id IN (?)", cards_id)
+    end
+
+    def total_harvest_time(board, card)
+      card_id, board_id = get_card_id(card), board.id
+      HarvestLog.total_time_tracked(board_id, card_id).sum(&:total_time)
+    end
+
+    def list_cards(lists, only_staged_and_live)
+      cards = Array.new
+      lists.each do |list|
+        cards << list.cards.map(&:id) if only_staged_and_live == list.finished?
+      end
+      cards
+    end
+
 end
