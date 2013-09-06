8a80d30e46d81226756571ab6ddaa0e9b92681af
Jonathan Salis <jtsseneca@hotmail.com>
2013-09-06 12:26:09 -0400

    card 2 - Removing unused methods from the models

--- app/models/harvest_trello.rb
+++ app/models/harvest_trello.rb
@@ -12,18 +12,4 @@ class HarvestTrello < ActiveRecord::Base
             :presence => true
 
   has_many :harvest_logs
-
-  def self.find_or_create(long_id, short_id, card_name, board_name, board_id)
-    if HarvestTrello.where(trello_card_long_id: long_id).first
-      puts "<--- Database record already exists --->"
-    else
-      HarvestTrello.create!(
-        trello_board_name: board_name,
-        trello_board_id: board_id,
-        trello_card_name: card_name,
-        trello_card_short_id: short_id,
-        trello_card_long_id: long_id)
-    end
-  end
-
 end
