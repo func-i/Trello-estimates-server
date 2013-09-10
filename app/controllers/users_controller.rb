e5977be23dda97e04bc863e90aee346e30a9c58d
coolmatt <themattemmons@gmail.com>
2013-09-10 14:11:40 -0400

    Card 2 - Small changes/fixes
    * Removed User dropdown menu from header
    * Removed unused function from UserController
    * Fixed estimations fixture
    * Fixed bug in HarvestTrello#show view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,9 @@
 class UsersController < ApplicationController
+
   #TODO: This can be improved later
   def show
     @user_email = current_user.find("member", params[:id]).email
-    @estimation_time_tracked_dif = Tasks::Calculator.avg_estimation_timetracked_by_developer
+    # @estimation_time_tracked_dif = Tasks::Calculator.avg_estimation_timetracked_by_developer
   end
+
 end
--- app/models/tasks/calculator.rb
+++ app/models/tasks/calculator.rb
@@ -1,12 +1,13 @@
 class Tasks::Calculator
-  def self.avg_estimation_timetracked_by_developer
 
+  # TODO: rewrite this method
+  def self.avg_estimation_timetracked_by_developer
     # query = "select ((sum(e.user_time)-sum(ht.total_time))/(count(e.id)+ count(ht.id))) as difference,
     #         ht.day from estimations e, harvest_logs ht where
     #         e.board_id = ht.board_id AND e.card_id = ht.card_id
     #         group by e.created_at,ht.day"
 
-    # this isn't working
     # ActiveRecord::Base.connection.execute(query)
   end
+
 end
