40e6cdb251237e246d8cfd25385b2ea34820ecb8
tfchang <tingfchang@gmail.com>
2016-01-06 10:33:29 -0500

    Refactor helpers

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -8,10 +8,10 @@ module UsersHelper
 
   def set_current_user(access_token)
     session[:user] = Trello::Client.new(
-        :consumer_key => Figaro.env.trello_developer_key,
-        :consumer_secret => Figaro.env.trello_developer_secret_key,
-        :oauth_token => access_token.token,
-        :oauth_token_secret => access_token.secret
+      consumer_key:       Figaro.env.trello_developer_key,
+      consumer_secret:    Figaro.env.trello_developer_secret_key,
+      oauth_token:        access_token.token,
+      oauth_token_secret: access_token.secret
     )
   end
 end
