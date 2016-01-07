6e75325b4de9c3a092bd0ee25f68c88762430597
tfchang <tingfchang@gmail.com>
2016-01-07 17:58:02 -0500

    parse_short_link in TrelloHelper

--- app/helpers/cards_helper.rb
+++ app/helpers/cards_helper.rb
@@ -2,10 +2,6 @@ module CardsHelper
 
   private
 
-  def parse_card_id(url)
-    url.split('/')[4]
-  end
-
   def linked_card_name(card)
     link_to "Card #{card.short_id} - #{truncate card.name, length: 65}", card.url, target: "_blank"
   end
--- app/helpers/trello_helper.rb
+++ app/helpers/trello_helper.rb
@@ -14,4 +14,11 @@ module TrelloHelper
       oauth_token_secret: access_token.secret
     )
   end
+
+  # parse out the shortLink of a board or card from its url
+  # the shortLink is used as board_id, card_id
+  def parse_short_link(url)
+    url.split('/')[4]
+  end
+
 end
