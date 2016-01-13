13634e62d5d09418b100a12839ba2cc97d14faa6
tfchang <tingfchang@gmail.com>
2016-01-13 11:26:06 -0500

    delete Trello and Harvest initializers

--- config/initializers/harvest.rb
+++
@@ -1,7 +0,0 @@
-# require "harvested"
-
-# HARVEST = Harvest.hardy_client(
-#   subdomain:  Figaro.env.harvest_subdomain,
-#   username:   Figaro.env.harvest_username,
-#   password:   Figaro.env.harvest_password
-# )
